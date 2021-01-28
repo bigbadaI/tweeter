5fcb3eb83348743a7d5ee2d18fc3aa47a50d47dd
BigBadAI <flashmefusion@hotmail.ca>
2021-01-28 02:05:16 +0000

    Add alert for no char or too many char

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,13 +5,24 @@ $(document).ready(function() {
 
   // eslint-disable-next-line no-undef
   $('textarea').on("keyup", function() {
+    let numLength = 140 - this.value.length;
     if ((140 - this.value.length) < 0) {
       $('.counter').addClass("negative");
     }
     if ((140 - this.value.length) >= 0) {
       $('.counter').removeClass("negative");
     }
+    if (numLength === 140 || numLength < 0) {
+      $("button").on("click", function(event) {
+        event.stopPropagation();
+        // event.preventDefault();
+        alert("Sorry your message doesn't fit the criteria");
+      });
+    }
+    
     $('output.counter').html(140 - this.value.length);
+    
   });
+
 
 });
