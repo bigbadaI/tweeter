5a0066b0a5898234e3e137fb1d43873dfb57b36b
BigBadAI <flashmefusion@hotmail.ca>
2021-01-28 03:46:53 +0000

    Fix issue if someone tries to over write app with an html tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,31 @@
  */
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
 
 
@@ -50,9 +50,11 @@ $(document).ready(function() {
 
   //changes the tweet object into html text for rendering on the page
   const createTweetElement = function(data) {
+    let words = data.content.text;
+    let newWords = words.replace(/(<([^>]+)>)/gi, "");
     let $tweet = $('<article></article');
     let newHeader = $(`<header><img src=${data.user.avatars}><h2>${data.user.handle}</h2>`);
-    let newBody = $(`<body><p>${data.content.text}</p></body>`);
+    let newBody = $(`<body><p>${newWords}</p></body>`);
     let breakLine = $('<hr>');
     let newFooter = $(`<div class="footer"><p>${data.created_at}</p>      
     <div class="footer-images">
@@ -74,7 +76,10 @@ $(document).ready(function() {
     $.ajax({
       type: "POST",
       url: 'http://localhost:8080/tweets',
-      data: $(this).serialize()
+      data: $(this).serialize(),
+      success: function() {
+        loadTweets();
+      }
     });
   });
 
